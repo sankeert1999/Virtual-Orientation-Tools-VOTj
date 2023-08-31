689b625ca758ac37a813e0084621d1cd6cecd5bb
sankeert1999 <sankeert1999@gmail.com>
2023-09-01 00:32:42 +0200

    Get orienation function

--- Utils.py
+++ Utils.py
@@ -3,6 +3,7 @@ from org.bytedeco.javacpp.opencv_core import Mat, MatVector, CvMat,Scalar,split,
 from org.bytedeco.javacpp.opencv_imgproc import findContours, RETR_LIST, CHAIN_APPROX_NONE, contourArea,moments,drawContours
 from ImageConverter import ImProcToMat,MatToImProc
 from ij import ImagePlus
+import math
 
 def getMax(myList):
     """
@@ -31,18 +32,25 @@ def detectContours(binary_masks):
     binary_masks -- Binary mask image containing object regions.
 
     Returns:
-    largest_contour -- The largest contour detected in the binary masks.
+    largest_contour_out -- The largest contour detected in the binary masks as a Mat.
     """
+    # Create a MatVector to store detected contours
     listContour = MatVector()
     findContours(binary_masks, listContour, RETR_LIST, CHAIN_APPROX_NONE)
+    
+    # Calculate the area of each detected contour and store in the 'area' list
     area = []
     for i in range(listContour.size()):
         area.append(contourArea(listContour.get(i)))
 
+    # Find the largest contour based on area
     max_value, max_value_index = getMax(area)
     largest_contour = listContour.get(max_value_index)
-    largest_contour_out=Mat()
-    largest_contour.convertTo(largest_contour_out,5)
+    
+    # Convert the largest contour to a new Mat
+    largest_contour_out = Mat()
+    largest_contour.convertTo(largest_contour_out, 5)  # Convert to desired data type
+    
     return largest_contour_out
 
 
@@ -64,8 +72,44 @@ def contourCenterExtractor(largest_contour):
     Com_y = int(contour_Moments.m01() / contour_Moments.m00())
     return Com_x, Com_y
    
-# Function to get orientation
-#def getOrientation(pts):
+def getOrientation(largest_contour):
+    """
+    Calculate the orientation angle of the largest contour.
+
+    Arguments:
+    largest_contour -- The largest contour detected.
+
+    Returns:
+    angle -- The orientation angle in degrees.
+    """
+    # Convert the largest_contour to a MatVector
+    largest_contour_MV = MatVector()
+    split(largest_contour, largest_contour_MV)
+    
+    # Determine the number of points in the contour
+    sz = largest_contour.total()
+    
+    # Create a Mat to store contour points for PCA
+    largest_contour_pca = Mat(sz, 2, 5)
+    
+    # Copy contour points to the Mat
+    largest_contour_MV.get(0).copyTo(largest_contour_pca.col(0))
+    largest_contour_MV.get(1).copyTo(largest_contour_pca.col(1))
+    
+    # Perform PCA to find mean, eigenvectors, and eigenvalues
+    mean_pca = Mat()
+    eigenvectors = Mat()
+    PCACompute(largest_contour_pca, mean_pca, eigenvectors)
+    
+    # Convert eigenvectors to a CvMat for indexing
+    eigenvectors_cvmat = CvMat(eigenvectors)
+    
+    # Calculate the angle of rotation based on eigenvectors
+    angle = math.atan2(eigenvectors_cvmat.get(0, 1), eigenvectors_cvmat.get(0, 0))
+    angle = int(math.degrees(angle))
+    
+    return angle
+
 	
     
 if __name__ in ['__main__', '__builtin__']:
@@ -75,28 +119,9 @@ if __name__ in ['__main__', '__builtin__']:
     binary_masks = imMat
     # Detect the largest contour in the binary mask
     largest_contour = detectContours(binary_masks)
-    largest_contour_MV=MatVector()
-    split(largest_contour,largest_contour_MV)
-    sz = largest_contour.total()
-    largest_contour_pca = Mat(sz,2,5)
-    largest_contour_MV.get(0).copyTo(largest_contour_pca.col(0))
-    largest_contour_MV.get(1).copyTo(largest_contour_pca.col(1))
-    #print(largest_contour_pca)
-    mean_pca = Mat()
-    eigenvectors = Mat()
-    PCACompute(largest_contour_pca,mean_pca,eigenvectors)
-    print(eigenvectors)
-    
-    #largest_contour_cv_mat=CvMat(largest_contour)
-    #sz = largest_contour_cv_mat.total()
-    #print(sz)
-    #largest_contour_pca = Mat(sz,2,5)
-    #print(largest_contour_pca)
-    #largest_contour_pca_cv_mat = CvMat(largest_contour_pca)
-    #print(largest_contour_pca_cv_mat.get(0,0,1))
-    #print(largest_contour_cv_mat.get(0,0,1))
-    #largest_contour_pca_cv_mat.put(0,0,1,largest_contour_cv_mat.get(0,0,1))
-    #print(largest_contour_pca_cv_mat.get(0,0,1))
+    angle = getOrientation(largest_contour)
+    print(angle)
+
     
 	
 
