04678d603b2f343efba10db229de039731fbefd8
sankeert1999 <sankeert1999@gmail.com>
2023-11-21 22:10:50 +0100

    Adding option for polarity

--- Fiji.app/scripts/Plugins/Virtual_Orientation_Tool/VO_(input_image).py
+++ Fiji.app/scripts/Plugins/Virtual_Orientation_Tool/VO_(input_image).py
@@ -4,10 +4,11 @@
 #@ String (choices={"Horizontal", "Vertical"}, label = "Orientation",style="listBox") orientation
 #@ String (choices={"Object_center", "Image_center"}, label = "Center of rotation",style="radioButtonHorizontal") center_of_rotation 
 #@ String (choices={"Yes", "No"}, label = "Enlarge Image",style="radioButtonHorizontal") enlarge
+#@ String (choices={"None","Left-Right/Top-Bottom", "Right-Left/Bottom-Top"}, label = "Object_Polarity",style="listBox") object_polarity
 
 from VOT_Utils import process_input_img,output_image_maker
 
-ip_list = process_input_img(img, mask, task, orientation, center_of_rotation, enlarge)
+ip_list = process_input_img(img, mask, task, orientation, center_of_rotation, enlarge,object_polarity)
 imp_out = output_image_maker(img, ip_list)
 imp_out.show()
 
