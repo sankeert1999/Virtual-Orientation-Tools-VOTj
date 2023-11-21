16e7e153a709e57bb24489fe1adb4768be6f4e97
sankeert1999 <sankeert1999@gmail.com>
2023-11-21 22:43:35 +0100

    Updated with Polarity

--- Fiji.app/scripts/Plugins/Virtual_Orientation_Tool/VO_(input_image).py
+++ Fiji.app/scripts/Plugins/Virtual_Orientation_Tool/VO_(input_image).py
@@ -1,16 +1,44 @@
-#@ ImagePlus (label="Select the image file") img 
-#@ ImagePlus (label="Select the mask file") mask 
-#@ String (choices={"Centering", "Rotation","Centering+Rotation"}, label = "Tasks", style="listBox") task
-#@ String (choices={"Horizontal", "Vertical"}, label = "Orientation",style="listBox") orientation
-#@ String (choices={"Object_center", "Image_center"}, label = "Center of rotation",style="radioButtonHorizontal") center_of_rotation 
-#@ String (choices={"Yes", "No"}, label = "Enlarge Image",style="radioButtonHorizontal") enlarge
-#@ String (choices={"None","Left-Right/Top-Bottom", "Right-Left/Bottom-Top"}, label = "Object_Polarity",style="listBox") object_polarity
+Win = GenericDialogPlus("Virtual Orientation Toolbar")
+Win.addImageChoice("Select the image file", prefs.get("Image","Choice")) 
+Win.addImageChoice("Select the mask file", prefs.get("Mask", "Choice")) 
+Win.addChoice("Tasks", ["Centering", "Rotation","Centering+Rotation"], prefs.get("Tasks","Centering+Rotation"))
+Win.addChoice("Orientation", ["Horizontal", "Vertical"], prefs.get("Orientation","Horizontal"))
+Win.addChoice("Center_Of_Rotation", ["Object_center", "Image_center"], prefs.get("Center_Of_Rotation","Image_center"))
+Win.addChoice("Enlarge", ["Yes", "No"], prefs.get("Enlarge","No")) 
+Win.addChoice("Object_Polarity", ["None", "Left-Right/Top-Bottom", "Right-Left/Bottom-Top"], prefs.get("Object_Polarity","None"))
+# Display a message asking users to cite the paper if they use the plugin.
+Win.addMessage("""If you use this plugin please cite:
+Cite paper""") 
 
-from VOT_Utils import process_input_img,output_image_maker
+# Provide a link to the plugin's documentation on GitHub.
+Win.addHelp("https://github.com/sankeert1999/Virtual_orienation_tool_FIJI/blob/main/README.md")  
 
-ip_list = process_input_img(img, mask, task, orientation, center_of_rotation, enlarge,object_polarity)
-imp_out = output_image_maker(img, ip_list)
-imp_out.show()
+# Display the GUI to the user.
+Win.showDialog()
+
+if Win.wasOKed(): 
+    img = Win.getNextImage()  
+    mask = Win.getNextImage() 
+    task = Win.getNextChoice()
+    orientation = Win.getNextChoice()
+    center_of_rotation = Win.getNextChoice()
+    enlarge = Win.getNextChoice()
+    object_polarity = Win.getNextChoice()
+    prefs.put("Image", img.getTitle()) 
+    prefs.put("Mask", mask.getTitle()) 
+    prefs.put("Tasks", task)
+    prefs.put("Orientation", orientation)
+    prefs.put("Center_Of_Rotation", center_of_rotation)
+    prefs.put("Enlarge", enlarge)
+    prefs.put("Object_Polarity", object_polarity)
+
+
+
+    from VOT_Utils import process_input_img,output_image_maker
+	
+    ip_list = process_input_img(img, mask, task, orientation, center_of_rotation, enlarge,object_polarity)
+    imp_out = output_image_maker(img, ip_list)
+    imp_out.show()
 
 
 
